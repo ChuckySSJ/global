2bc576a8159980787a1bd582125fe39fe153199b
ChuckySSJ <ChuckySSJ.pepito_chivaz10@hotmail.com>
2021-04-14 08:02:44 -0500

    primera

---
+++ tabla.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mio</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<h1 id="consultar">Jose Manuel Lopez Renteria</h1>
+	<div>
+		<table id="contenedor">
+	 	</table>
+	 </div>
+<script> 
+const xhttp = new XMLHttpRequest();
+xhttp.open('GET','alumnos.xml',true);
+xhttp.responseType = 'xml'; 
+xhttp.send(); 
+xhttp.onreadystatechange = function(){
+	const mio = xhttp.response; 
+	console.log(mio);
+	let resultado = "<th>Nombre</th><th>Carrera</th><th>Cuatri</th>";
+if(this.readyState == 4 && this.status == 200){
+	
+for (var i = 0; i<mio.length; i++){
+	resultado+="<tr><td>"+mio[i].nombre+ "</td>";
+	resultado+="<td>"+mio[i].carrera+ "</td>";
+	resultado+="<td>"+mio[i].cuatri+ "</td></tr>";
+}
+}
+resultado = resultado + "";
+document.getElementById("contenedor").innerHTML=resultado;
+}
+</script>
+</body>
+</html>
