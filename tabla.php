c0109edda9a3c67c81f94a31d02f4033d0167af8
ChuckySSJ <ChuckySSJ.pepito_chivaz10@hotmail.com>
2021-04-14 09:59:31 -0500

    tercera

--- tabla.php
+++ tabla.php
@@ -13,23 +13,32 @@
 	 	</table>
 	 </div>
 <script> 
+	function cargarCatalogo(){
 const xhttp = new XMLHttpRequest();
-xhttp.open('GET','alumnos.xml',true); 
-xhttp.send(); 
-xhttp.onreadystatechange = function(){
+xhttp.onreadystatechange = function(){ 
+	if(this.readyState == 4 && this.status == 200){
+cargarXML(this);
+}
+};
+xhttp.open('GET','alumnos.xml',true);
+xhttp.send();
+ }
+
+    function cargarXML(xml){ 
 	const mio = xhttp.responseXML; 
-	console.log(mio);
-	let resultado = "<th>Nombre</th><th>Carrera</th><th>Cuatri</th><th>suma</th>";
-if(this.readyState == 4 && this.status == 200){
+	var tabla = "<th>Nombre</th><th>Carrera</th><th>Cuatri</th><th>suma</th>";
+	var alumnos = mio.getElementsByTagName("alumno");
 	
 for (var i = 0; i<mio.length; i++){
-	resultado+="<tr><td>"+mio[i].nombre+ "</td>";
-	resultado+="<td>"+mio[i].carrera+ "</td>";
-	resultado+="<td>"+mio[i].cuatri+ "</td></tr>";
+	tabla+="<tr><td>"; 
+	tabla+= alumnos[i].getElementsByTagName("nombre")[0].textContent;
+	tabla+="<td><td>"; 
+	tabla+= alumnos[i].getElementsByTagName("carrera")[0].textContent;
+	tabla+="<td><td>"; 
+	tabla+= alumnos[i].getElementsByTagName("cuatri")[0].textContent;
+	tabla+="</td></tr>"; 
 }
-}
-
-document.getElementById("contenedor").innerText=this.responseText;
+document.getElementById("contenedor").innerHTML = tabla;
 }
 </script>
 </body>
